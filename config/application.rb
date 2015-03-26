18c412a6689b178cc0210e8f0ee51443855b4c33
ivanvr <github@ivanyvenian.com>
2015-03-26 16:44:55 -0600

    Disable asset generation.

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module BrainIntelligence
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators.javascripts = false
+    config.generators.stylesheets = false
   end
 end
 
