517c6f1438a8deea9ce8561c5a66c9d64bb388e5
ivanvr <github@ivanyvenian.com>
2015-04-05 23:57:25 -0500

    Adjust provisioning scripts.

--- provision/roles/passenger/templates/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /etc/profile.d/rbenv.sh
-gem install rack --no-doc
-cd /opt/passenger-5.0.5
-bin/passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download --languages=ruby
