7129c3016cf71e6f33e6a85d64038395addd41cd
ivanvr <github@ivanyvenian.com>
2015-04-06 20:51:30 -0500

    Temporarily disable video admin. While the video attributes, hosting, etc. is defined.

--- app/admin/video.rb
+++ app/admin/video.rb
@@ -1,20 +1,20 @@
-ActiveAdmin.register Video do
-  include AdminResource
-
-  config.filters = false
-
-  index do
-  end
-
-  show do
-
-  end
-
-  form do |f|
-    inputs do
-      input :file, as: :file, input_html: { data: { url: uploads_path }}
-    end
-
-    actions
-  end
-end
+# ActiveAdmin.register Video do
+#   include AdminResource
+#
+#   config.filters = false
+#
+#   index do
+#   end
+#
+#   show do
+#
+#   end
+#
+#   form do |f|
+#     inputs do
+#       input :file, as: :file, input_html: { data: { url: uploads_path }}
+#     end
+#
+#     actions
+#   end
+# end
