c5581c3ae756c9917dcdaa5fd9a6fb999fdea96b
ivanvr <github@ivanyvenian.com>
2015-04-06 22:49:29 -0500

    Workaround for mina issue. It seems the rails tasks run in the `current` dir before it's been linked to the new release.

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,12 +58,12 @@ task deploy: :environment do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    invoke :'rails:db_seed'
     invoke :'bower:install'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     to :launch do
+      invoke :'rails:db_seed'
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
     end
